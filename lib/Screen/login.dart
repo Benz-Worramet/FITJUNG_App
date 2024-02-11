3b0c49039d412a3adf5166da4a1438e9f4e57ee1
teamchz <teamchz@gmail.com>
2024-02-11 23:27:16 +0700

    Add login Screen

--- lib/Screen/login.dart
+++ lib/Screen/login.dart
@@ -35,78 +35,188 @@ class _LooginScreenState extends State<LooginScreen> {
             );
           } else if (snapshot.connectionState == ConnectionState.done) {
             return Scaffold(
-              appBar: AppBar(
-                title: Text("Log in"),
-              ),
               body: Container(
-                  child: Padding(
-                padding: const EdgeInsets.all(30.0),
-                child: Form(
-                    key: formKey,
-                    child: SingleChildScrollView(
-                      child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+                color: Colors.black87,
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.2,
+                      child: const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("Log In", style: TextStyle(fontSize: 50,
+                              fontWeight: FontWeight.bold, color: Colors.orange))
+                        ],
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 0, 0, 25),
+                          child: Container(
+                            width: MediaQuery.of(context).size.width * 0.8,
+                            height: MediaQuery.of(context).size.height * 0.055,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            child: const Row(
+                              children: [
+                                Padding(
+                                  padding: EdgeInsetsDirectional.symmetric(horizontal: 10),
+                                  child: Icon(Icons.email_rounded, color: Colors.black87,),
+                                ),
+                                Text("Email", style: TextStyle(
+                                    color: Colors.black38,
+                                    fontSize: 20
+                                ),)
+                              ],
+                            )
+                            ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+                          child: Container(
+                              width: MediaQuery.of(context).size.width * 0.8,
+                              height: MediaQuery.of(context).size.height * 0.055,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                            child: const Row(
+                              children: [
+                                Padding(
+                                  padding: EdgeInsetsDirectional.symmetric(horizontal: 10),
+                                  child: Icon(Icons.lock, color: Colors.black87,),
+                                ),
+                                Text("Password", style: TextStyle(
+                                  color: Colors.black38,
+                                  fontSize: 20
+                                ),)
+                              ],
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.45,
+                          height: MediaQuery.of(context).size.height * 0.075,
+                          decoration: BoxDecoration(
+                            color: Colors.redAccent,
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: const Center(
+                            child: Text("Log In", style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25
+                            ),),
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.all(20),
+                          child: Text("Forgot your password?", style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            decoration: TextDecoration.underline,
+                            decorationColor: Colors.white
+                          ),),
+                        ),
+                        Row(
                           children: [
-                            Text("Email", style: TextStyle(fontSize: 20)),
-                            TextFormField(
-                              validator: MultiValidator([
-                                RequiredValidator(
-                                    errorText: "Please enter your email"),
-                                EmailValidator(
-                                    errorText: "please enter an email address")
-                              ]),
-                              keyboardType: TextInputType.emailAddress,
-                              onSaved: (email) {
-                                profile.email = email!;
-                              },
+                            Container(
+                              width: MediaQuery.of(context).size.width * 0.4,
+                              height: 5,
+                              color: Colors.white,
                             ),
                             SizedBox(
-                              height: 15,
+                              width: MediaQuery.of(context).size.width * 0.2,
+                              child: const Center(
+                                child: Text("or", style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16
+                                ),),
+                              ),
                             ),
-                            Text("Password", style: TextStyle(fontSize: 20)),
-                            TextFormField(
-                              validator: RequiredValidator(
-                                  errorText: "Please enter your Password"),
-                              obscureText: true,
-                              onSaved: (password) {
-                                profile.password = password!;
-                              },
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 20),
-                              child: SizedBox(
-                                  width: double.infinity,
-                                  child: ElevatedButton(
-                                    child: Text("Log in",
-                                        style: TextStyle(fontSize: 20)),
-                                    onPressed: () async {
-                                      if (formKey.currentState!.validate()) {
-                                        formKey.currentState!.save();
-                                        try {
-                                          await FirebaseAuth.instance
-                                              .signInWithEmailAndPassword(
-                                                  email: profile.email,
-                                                  password: profile.password)
-                                              .then((value) {
-                                            formKey.currentState!.reset();
-                                            Navigator.pushReplacement(context,
-                                                MaterialPageRoute(
-                                                    builder: (context) {
-                                              return WelcomeScreen();
-                                            }));
-                                          });
-                                        } on FirebaseAuthException catch (e) {
-                                          Fluttertoast.showToast(
-                                              msg: e.message.toString(),
-                                              gravity: ToastGravity.CENTER);
-                                        }
-                                      }
-                                    },
-                                  )),
+                            Container(
+                              width: MediaQuery.of(context).size.width * 0.4,
+                              height: 5,
+                              color: Colors.white,
                             )
-                          ]),
-                    )),
-              )),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsetsDirectional.symmetric(vertical: 30),
+                          child: Container(
+                            width: MediaQuery.of(context).size.width * 0.75,
+                            height: MediaQuery.of(context).size.height * 0.065,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(25),
+                              border: Border.all(color: Colors.white),
+                            ),
+                            child: Row(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsetsDirectional.symmetric(horizontal: 25),
+                                  child: Image.asset('assets/image/google_icon.png', height: 30,),
+                                ),
+                                const Center(
+                                  child: Text("Log In With Google", style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w600
+                                  ),),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.75,
+                          height: MediaQuery.of(context).size.height * 0.065,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(25),
+                            border: Border.all(color: Colors.white),
+                          ),
+                          child: Row(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsetsDirectional.symmetric(horizontal: 25),
+                                child: Image.asset('assets/image/facebook_icon.png', height: 35,),
+                              ),
+                              const Center(
+                                child: Text("Log In With Facebook", style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w600
+                                ),),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsetsDirectional.symmetric(vertical: 40),
+                          child: Container(
+                            width: MediaQuery.of(context).size.width * 1,
+                            height: MediaQuery.of(context).size.height * 0.001,
+                            color: Colors.white38 ,
+                          ),
+                        ),
+                        const Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("Don't have an account ? ", style: TextStyle(
+                              color: Colors.white60,
+                            )),
+                            Text("Sign up", style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold
+                            )),
+                          ],
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
             );
           }
           return Scaffold(
